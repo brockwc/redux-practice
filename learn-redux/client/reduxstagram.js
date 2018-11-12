c3e65c13695eea8831d427974bb43b0c06b59a50
brockwc <brock.whitbread@gmail.com>
2018-11-11 17:06:20 -0800

    user cloneElement over createElement

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -5,6 +5,26 @@ import { render } from 'react-dom';
 // Import CSS
 import css from './styles/style.styl';
 
-import Main from './components/Main.js';
+// import components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+
+const router = (
+  <Provider store={ store }>
+    <Router history={ history }>
+      <Route path='/' component={ Main }>
+        <IndexRoute component={ PhotoGrid }></IndexRoute>
+        <Route path='/view/:postId' component={ Single }></Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+render(router, document.getElementById('root'));
